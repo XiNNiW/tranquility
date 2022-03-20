f2f39cdc23644b70a5ea9ebf4310b44a305063ad
David Minnix <david.minnix@protonmail.com>
2022-03-20 14:36:45 -0400

    implemented a few more operators for fractional... I may need a few more in order to play with the rest of the pattern type

--- src/fractional.lua
+++ src/fractional.lua
@@ -1,5 +1,21 @@
 require('math')
 
+-- this is a quick and dirty port of python's Fraction library pulling in only the things i need to get a first version working
+-- this should probably all be C calls instead
+--TODO
+-- from_float
+-- from_decimal
+-- mod
+-- pow
+-- rpow
+-- pos
+-- neg
+-- abs?
+-- trunc?
+-- floor?
+-- ciel?
+-- round?
+
 local function gcd(a, b)
 	return b==0 and a or gcd(b,a%b)
 end
@@ -14,9 +30,9 @@ function Fraction:create (o)
 end
 
 function Fraction:new (
-    numerator, 
-    denominator, 
-    shouldNormalize 
+    numerator,
+    denominator,
+    shouldNormalize
 )
     local n = numerator or 0
     local d = denominator or 1
@@ -28,7 +44,7 @@ function Fraction:new (
         n = n//g
         d = d//g
     end
-    
+
     local f = Fraction:create{_numerator = n, _denominator = d}
     return f
 end
@@ -41,17 +57,138 @@ function Fraction:denominator ()
     return self._denominator
 end
 
---TODO
--- from_float
--- from_decimal
--- __add
--- __sub
--- __mul
--- __div
-
 function Fraction:__add (f2)
-    --n = self:numerator() + f2:numerator()
-    --d = self:denominator() + f2:denominator()
-    return Fraction:new(3,4)
+
+    -- """a + b"""
+    -- na, da = a.numerator, a.denominator
+    -- nb, db = b.numerator, b.denominator
+    -- g = math.gcd(da, db)
+    -- if g == 1:
+    --     return Fraction(na * db + da * nb, da * db, _normalize=False)
+    -- s = da // g
+    -- t = na * (db // g) + nb * s
+    -- g2 = math.gcd(t, g)
+    -- if g2 == 1:
+    --     return Fraction(t, s * db, _normalize=False)
+    -- return Fraction(t // g2, s * (db // g2), _normalize=False)
+
+    local na = self:numerator()
+    local nb = f2:numerator()
+    local da = self:denominator()
+    local db = f2:denominator()
+    local g = gcd(da,db)
+
+    if g == 1 then
+        return Fraction:new(na*db + da*nb, da*db, false)
+    end
+    local s = da//g
+    local t = na*(db//g) + nb*s
+    local g2 = gcd(t,g)
+    if g2 == 1 then
+        return Fraction:new(t, s*db, false)
+    end
+    return Fraction:new(t//g2, s*(db//g2), false)
 end
 
+function Fraction:__sub (f2)
+    -- """a - b"""
+    --     na, da = a.numerator, a.denominator
+    --     nb, db = b.numerator, b.denominator
+    --     g = math.gcd(da, db)
+    --     if g == 1:
+    --         return Fraction(na * db - da * nb, da * db, _normalize=False)
+    --     s = da // g
+    --     t = na * (db // g) - nb * s
+    --     g2 = math.gcd(t, g)
+    --     if g2 == 1:
+    --         return Fraction(t, s * db, _normalize=False)
+    --     return Fraction(t // g2, s * (db // g2), _normalize=False)
+    local na = self:numerator()
+    local nb = f2:numerator()
+    local da = self:denominator()
+    local db = f2:denominator()
+    local g = gcd(da,db)
+
+    if g == 1 then
+        return Fraction:new(na*db - da*nb, da*db, false)
+    end
+    local s = da//g
+    local t = na*(db//g) - nb*s
+    local g2 = gcd(t,g)
+    if g2 == 1 then
+        return Fraction:new(t, s*db, false)
+    end
+    return Fraction:new(t//g2, s*(db//g2), false)
+
+end
+
+function Fraction:__div (f2)
+    -- # Same as _mul(), with inversed b.
+    -- na, da = a.numerator, a.denominator
+    -- nb, db = b.numerator, b.denominator
+    -- g1 = math.gcd(na, nb)
+    -- if g1 > 1:
+    --     na //= g1
+    --     nb //= g1
+    -- g2 = math.gcd(db, da)
+    -- if g2 > 1:
+    --     da //= g2
+    --     db //= g2
+    -- n, d = na * db, nb * da
+    -- if d < 0:
+    --     n, d = -n, -d
+    -- return Fraction(n, d, _normalize=False)
+    local na = self:numerator()
+    local nb = f2:numerator()
+    local da = self:denominator()
+    local db = f2:denominator()
+    local g1 = gcd(na,nb)
+    if g1 > 1 then
+        na = na//g1
+        nb = nb//g1
+    end
+    local g2 = gcd(db, da)
+    if g2 > 1 then
+        da = da//g2
+        db = db//g2
+    end
+    local n = na*db
+    local d = nb*da
+    if d < 0 then
+        n = -n
+        d = -d
+    end
+
+    return Fraction:new(n,d,false)
+end
+
+function Fraction:__mul (f2)
+    -- """a * b"""
+    -- na, da = a.numerator, a.denominator
+    -- nb, db = b.numerator, b.denominator
+    -- g1 = math.gcd(na, db)
+    -- if g1 > 1:
+    --     na //= g1
+    --     db //= g1
+    -- g2 = math.gcd(nb, da)
+    -- if g2 > 1:
+    --     nb //= g2
+    --     da //= g2
+    -- return Fraction(na * nb, db * da, _normalize=False)
+    local na = self:numerator()
+    local nb = f2:numerator()
+    local da = self:denominator()
+    local db = f2:denominator()
+    local g1 = gcd(na,db)
+    if g1 > 1 then
+        na = na//g1
+        db = db//g1
+    end
+    local g2 = gcd(nb,da)
+    if g2 > 1 then
+        nb = nb//g2
+        da = da//g2
+    end
+
+    return Fraction:new(na*nb,da*db,false)
+end
--- test/lunacy_test.lua
+++ test/lunacy_test.lua
@@ -1,6 +1,7 @@
 ---@diagnostic disable: different-requires
 local lu = require('test/luaunit/luaunit')
 require('src/fractional')
+
 function TestFractional__create()
     local f = Fraction:create()
     lu.assertEquals(f:numerator(), 0)
@@ -36,6 +37,76 @@ function TestFractional__add()
     local f1 = Fraction:new(1,2)
     local f2 = Fraction:new(1,2)
     lu.assertEquals(f1+f2, Fraction:new(1))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(1,3)
+    -- 3/6 + 2/6
+    lu.assertEquals(f1+f2, Fraction:new(5,6))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(-1,3)
+    -- 3/6 + -2/6
+    lu.assertEquals(f1+f2, Fraction:new(1,6))
 end
+
+function TestFractional__sub()
+    local f1 = Fraction:new(1,2)
+    local f2 = Fraction:new(1,2)
+    lu.assertEquals(f1-f2, Fraction:new(0))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(1,3)
+    -- 3/6 - 2/6
+    lu.assertEquals(f1-f2, Fraction:new(1,6))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(-1,3)
+    -- 3/6 - -2/6
+    lu.assertEquals(f1-f2, Fraction:new(5,6))
+end
+
+function TestFractional__mult()
+    local f1 = Fraction:new(1,2)
+    local f2 = Fraction:new(1,2)
+    lu.assertEquals(f1*f2, Fraction:new(1,4))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(1,3)
+    lu.assertEquals(f1*f2, Fraction:new(1,6))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(-1,3)
+    lu.assertEquals(f1*f2, Fraction:new(-1,6))
+end
+
+function TestFractional__div()
+    local f1 = Fraction:new(1,2)
+    local f2 = Fraction:new(1,2)
+    lu.assertEquals(f1/f2, Fraction:new(1))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(1,3)
+    lu.assertEquals(f1/f2, Fraction:new(3,2))
+    f1 = Fraction:new(1,2)
+    f2 = Fraction:new(-1,3)
+    lu.assertEquals(f1/f2, Fraction:new(-3,2))
+end
+
+
+function TestFractional__mod()
+    local f1 = Fraction:new(1,2)
+    local f2 = Fraction:new(2,3)
+    lu.assertEquals(f1%f2, Fraction:new(1,3))
+    f1 = Fraction:new(3,4)
+    f2 = Fraction:new(2,3)
+    -- 9/12 % 8/12 = 1/12
+    lu.assertEquals(f1%f2, Fraction:new(1,12))
+    
+end
+
+function TestFractional__pow()
+    local f1 = Fraction:new(1,4)
+    local f2 = Fraction:new(1,2)
+    lu.assertEquals(f1^f2, Fraction:new(1,2))
+end
+
+function TestFractional__neg()
+    local f1 = Fraction:new(1,4)
+    lu.assertEquals(-f1, Fraction:new(-1,4))
+end
+
 
 os.exit( lu.LuaUnit.run() )
