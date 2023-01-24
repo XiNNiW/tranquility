549fc6bd7120a1bc1c10afc47bfab175cf7681aa
XiNNiW <david.minnix@protonmail.com>
2023-01-24 10:23:07 -0500

    cleaning up GPL comments

--- src/tranquility/stream.lua
+++ src/tranquility/stream.lua
@@ -1,8 +1,37 @@
+--[[
+Copyright (C) 2023 David Minnix
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]--
 require("tranquility/pattern")
-require("losc")
-require("losc.plugins.udp-socket")
+local losc = require'losc'
+local plugin = require'losc.plugins.udp-socket'
+
+local udp = plugin.new {sendAddr = 'localhost', sendPort = 9000}
+local osc = losc.new {plugin = udp}
+
+-- Create a message
+local message = osc.new_message {
+  address = '/foo/bar',
+  types = 'ifsb',
+  123, 1.234, 'hi', 'blobdata'
+}
+
+-- Send it over UDP
 
 
 function FirstOsc()
+    osc:send(message)
     return 2
 end
